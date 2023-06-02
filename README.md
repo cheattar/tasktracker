6ce2b4cf339c1363a3b44b693f35c2562f2290b1
Anna Dębowska <cheattar@tuta.io>
2023-06-02 21:43:51 +0200

    getting there!

---
+++ README.md
@@ -0,0 +1,9 @@
+# TaskTracker  - an over engineered to do list.
+I mean that header probably tells you everything you need to know about this program.
+Most beginners write an simple to-do list as one of their first projects, I decided to write my own, and the result was this... Yea, not pretty is it?
+Since summer was coming around the corner, and I had lots of free time, and I was feeling pretty bored and I didn't want to watch YouTube all day I made this monstrosity.
+
+# How to build
+You'll need an C++11 compiler, CMake and a reason to compile this. This program also uses Niels Lohmann's JSON library, and fmtlib, but CMake pulls them for you during config time (FetchContent).
+
+No special instructions is needed to build this program, just clone this via git, `cmake .`and `make`.
--- tasktracker.cpp
+++ tasktracker.cpp
@@ -1,6 +1,6 @@
 #include <fstream>
 #include <iostream>
-#include <sstream>
+#include <fmt/core.h>
 #include <string>
 #include <vector>
 #include <nlohmann/json.hpp>
@@ -9,56 +9,105 @@
 
 using json = nlohmann::json;
 
+// Copied from: https://stackoverflow.com/a/52895729
+void clear() {
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
+
 namespace tasks {
     struct task {
-        int id; 
-        std::string name;
-        std::string contents;
+        int id;
+        std::string description;
+        int urgency;
     };
+
+    /* Urgency map:
+        1: low,
+        2: moderate,
+        3: medium,
+        4: high,
+        5: critical 
+    */
 
     void to_json(json& j, const task& t) {
         //j = json{{ "id", t.id}, {"name", t.name}, {"contents", t.contents}};
         j["id"] = t.id;
-        j["name"] = t.name;
-        j["contents"] = t.contents;
+        j["description"] = t.description;
+        j["urgency"] = t.urgency;
     }
     void from_json(const json& j, task t) {
         j.at("id").get_to(t.id);
-        j.at("name").get_to(t.name);
-        j.at("contents").get_to(t.contents);
+        j.at("description").get_to(t.description);
+        j.at("urgency").get_to(t.urgency);
     }
 
 }
 
-void newTask(std::vector<tasks::task>& taskVector) {
-    std::cout << "Please enter your task name.\n? "; std::string utn; std::getline(std::cin, utn);
-    std::cout << "Please enter your task contents.\n? "; std::string utc; std::getline(std::cin, utc);
-    
+void addTask(std::vector<tasks::task>& taskVector) {
+    fmt::print("Please enter your task.\n? "); std::string utn; std::getline(std::cin, utn);
+    fmt::print("From a scale from 1 to 5, how urgent is your task?\n? "); int utu; std::cin >> utu;
    if (taskVector.empty()){
-       tasks::task t = { 0, utn, utc };
+       tasks::task t = { 0, utn, utu};
        taskVector.push_back(t);
     }
    else {
        int id = taskVector.back().id + 1; 
-       tasks::task t = { id, utn, utc };
+       tasks::task t = { id, utn, utu};
        taskVector.push_back(t);
    }
 }
 
-
+void viewTasks(std::vector<tasks::task>& taskVector) {
+    if (taskVector.size() == 0) {
+        fmt::print("No tasks to show.\n");
+        return;
+    }
+    
+   fmt::print("|{:^9}|{:^9}|{:^9}|\n", "No.", "Task", "Urgency");
+   for (int i = 0; i <= taskVector.back().id; i++) {
+        fmt::print("|{:^9}|{:^9}|{:^9}|\n", taskVector.at(i).id, taskVector.at(i).description, taskVector.at(i).urgency);
+    }
+}
 
 int main() {
-    std::vector<tasks::task> taks;
+    std::vector<tasks::task> taskList;
     
-    newTask(taks);
-    newTask(taks);
+    while (true) {
+        fmt::print("Select option: (A)dd task, (V)iew tasks, E(x)it.\n? ");
+        std::string usrChoice; std::getline(std::cin, usrChoice);
+        
+        if (usrChoice == "A" || usrChoice == "a") {
+            addTask(taskList);
+        }
+        else if (usrChoice == "V" || usrChoice == "v") {
+            viewTasks(taskList);
+        }
+        else if (usrChoice == "X" || usrChoice == "x") {
+            break;
+        }
+        else {
+            fmt::print("Incorrect option. Please try again.\n");
+        }
+    }
 
-    json j = taks;
+    json j = taskList;
 
     std::cout << j << '\n';
 
+    fmt::print("Saving tasks file...");
+
     std::ofstream file("tasks.json");
     file << j;
+
+    fmt::print("Good bye.");
 
     return 0;
 }
