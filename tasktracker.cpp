c9fce32b1c4d13a2ee13b6dcfbf2b1ee30b04a31
Anna Dębowska <cheattar@tuta.io>
2023-06-08 21:24:24 +0200

    added deleting, almost how i want it...

--- tasktracker.cpp
+++ tasktracker.cpp
@@ -51,18 +51,11 @@ namespace tasks {
 
 }
 
-void addTask(std::vector<tasks::task>& taskVector) {
-    fmt::print("Please enter your task.\n? "); std::string utn; std::getline(std::cin, utn);
-    fmt::print("From a scale from 1 to 5, how urgent is your task?\n? "); int utu; std::cin >> utu;
-   if (taskVector.empty()){
-       tasks::task t = { 0, utn, utu};
-       taskVector.push_back(t);
-    }
-   else {
-       int id = taskVector.back().id + 1; 
-       tasks::task t = { id, utn, utu};
-       taskVector.push_back(t);
-   }
+void rearrangeIDs(std::vector<tasks::task>& taskVector) {
+    int newID = 1;
+        for (auto& task : taskVector) {
+            task.id = newID++;
+        }
 }
 
 void viewTasks(std::vector<tasks::task>& taskVector) {
@@ -72,17 +65,52 @@ void viewTasks(std::vector<tasks::task>& taskVector) {
     }
     
    fmt::print("|{:^9}|{:^9}|{:^9}|\n", "No.", "Task", "Urgency");
-   for (int i = 0; i <= taskVector.back().id; i++) {
-        fmt::print("|{:^9}|{:^9}|{:^9}|\n", taskVector.at(i).id, taskVector.at(i).description, taskVector.at(i).urgency);
+   for (const auto& task : taskVector) {
+        fmt::print("|{:^9}|{:^9}|{:^9}|\n", task.id, task.description, task.urgency);
+    }
+}
+
+void addTask(std::vector<tasks::task>& taskVector) {
+    fmt::print("Please enter your task.\n? "); std::string utn; std::getline(std::cin, utn);
+    fmt::print("From a scale from 1 to 5, how urgent is your task?\n? "); int utu; std::cin >> utu;
+    if (taskVector.empty()){
+       tasks::task t = { 0, utn, utu};
+       taskVector.push_back(t);
+    }
+    else {
+       int id = taskVector.back().id + 1; 
+       tasks::task t = { id, utn, utu};
+       taskVector.push_back(t);
+   }
+   // Clearing cin here fixes an bug with the if statements down below...
+   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void deleteTask(std::vector<tasks::task>& taskVector) {
+    viewTasks(taskVector);
+
+    fmt::print("Please input which task you want to remove.\n? "); std::string inpIDstr; std::getline(std::cin, inpIDstr);
+    int inpID = std::stoi(inpIDstr);
+
+    if (inpID >= 1 && inpID <= taskVector.size()) {
+        // Find the task to delete using an iterator
+        auto it = taskVector.begin() + (inpID - 1);
+        taskVector.erase(it);
+
+        // Rearrange IDs so that they match up with vector size
+        rearrangeIDs(taskVector);
+
+        fmt::print("Task deleted successfully.\n");
+    } 
+    else {
+        fmt::print("Invalid task ID. Deletion aborted.\n");
     }
 }
 
 void loadTasks(std::vector<tasks::task>& taskList) {
     std::ifstream json_file("tasks.json");
     if (json_file) {
-        //std::cout << json_file.rdbuf();
         json j = json::parse(json_file);
-        //std::cout << j.dump(4) << std::endl;
         taskList = j.get<std::vector<tasks::task>>();
         json_file.close();
     }
@@ -90,28 +118,39 @@ void loadTasks(std::vector<tasks::task>& taskList) {
         fmt::print("tasks.json file not found in current directory or it doesn't exist!\nCreating new one...\n");
         std::vector<tasks::task> taskList;
     }
-    
 }
 
 int main() {
     fmt::print("Welcome to TaskTracker!\n");
+    
     std::vector<tasks::task> taskList;
     loadTasks(taskList);
+    
     while (true) {
-        start:
-        fmt::print("Select option: (A)dd task, (V)iew tasks, E(x)it.\n? ");
+        fmt::print("Select option: (A)dd task, (D)elete task, (V)iew tasks, E(x)it.\n? ");
         std::string usrChoice; std::getline(std::cin, usrChoice);
         
         if (usrChoice == "A" || usrChoice == "a") {
-            addTask(taskList);
+            clear(); 
+            addTask(taskList); 
+            rearrangeIDs(taskList); // We rearrange ID's so they look pretty on print, 
+                                                // and it's better when delete a task, 
+                                                // because human start counting at 1,
+                                                // arrays/vectors at 0 (unless you're programming in Lua or MATLAB...)
+        }
+        else if (usrChoice == "D" || usrChoice == "d") {
+            clear(); 
+            deleteTask(taskList);
         }
         else if (usrChoice == "V" || usrChoice == "v") {
+            clear(); 
             viewTasks(taskList);
         }
         else if (usrChoice == "X" || usrChoice == "x") {
             break;
         }
         else {
+            clear(); 
             fmt::print("Incorrect option. Please try again.\n");
         }
     }
@@ -122,10 +161,10 @@ int main() {
 
     if (usrExit == "Y" || usrExit == "y") {
         json j = taskList;
-        //std::cout << j << '\n';
         fmt::print("Saving tasks file...\n");
         std::ofstream file("tasks.json");
         file << j;
+        file.close();
     }
     else if(usrExit == "N" || usrExit == "n") {
         goto exit;
